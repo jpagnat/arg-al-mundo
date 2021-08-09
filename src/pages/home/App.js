2dad56ebddfa3d65716ed7a61c3de59cdde15ea5
jpagnat <pagnat.jm@gmail.com>
2021-08-09 18:27:27 -0300

    Correcion GH pages

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -12,7 +12,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <NavBar />
       <Switch>
         <Route exact path="/arg-al-mundo">
